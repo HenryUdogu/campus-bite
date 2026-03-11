02fb0a2b9b746feeba2ccfbe7a5900ebaad59d05
Henry Udogu <chibuezeudogu8@gmail.com>
2026-03-11 16:02:42 +0100

    commit7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,9 +37,11 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <button className="bg-orange-400 flex items-center gap-2 rounded-2xl px-4 py-2">
-                        <p className="text-white">Deliver with us</p>
-                        <ArrowRight className="text-white" />
+                    <button
+                        className="flex bg-orange-400 rounded-2xl h-10 items-center px-4 gap-3 text-white whitespace-nowrap"
+                        onClick={() => navigate("/signup")}
+                    >
+                        Order Now <ArrowRight size={18} />
                     </button>
                 </div>
 
