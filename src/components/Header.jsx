11734f93818acb3e5ce0cdbbea7ac52418501555
Y-Ice <isaacyakubu544@gmail.com>
2026-03-11 13:45:45 +0100

    commit3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -63,10 +63,12 @@ const Header = () => {
               About Us
             </a>
 
-            <button className="bg-orange-400 flex items-center gap-2 rounded-2xl px-4 py-2">
-              <p className="text-white">Deliver with us</p>
-              <ArrowRight className="text-white" />
-            </button>
+            <button
+              className="flex bg-orange-400 rounded-2xl h-10 items-center px-4 gap-3 text-white whitespace-nowrap"
+              onClick={() => navigate("/signup")}
+            >
+              Order Now <ArrowRight size={18} />
+            </button> 
           </div>
         )}
       </nav>
